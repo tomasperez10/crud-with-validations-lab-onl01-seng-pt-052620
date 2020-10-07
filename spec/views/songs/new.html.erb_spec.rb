74ec8b644142929c020ef53cd0be6e04a0081b42
Tomas Perez <tomasitopg6@gmail.com>
2020-10-07 23:29:06 +0000

    Automatically backed up by Learn

--- spec/views/songs/new.html.erb_spec.rb
+++ spec/views/songs/new.html.erb_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe 'songs/new', type: :feature do
     expect(form.find('input#song_release_year')[:name]).to eq('release_year')
     expect(form.find('input#song_released')[:name]).to eq('released')
     expect(form.find('input#song_genre')[:name]).to eq('genre')
-    expect(form.find('input#song_artist_name')[:name]).to eq('song[artist_name]')
+    expect(form.find('input#song_artist_name')[:name]).to eq('artist_name')
   end
 end
