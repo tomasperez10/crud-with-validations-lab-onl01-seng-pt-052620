0865d809f3506fd9f95ab9ca22c111f7bf4ed005
Tomas Perez <tomasitopg6@gmail.com>
2020-10-07 23:17:37 +0000

    Automatically backed up by Learn

--- db/migrate/20201007231708_create_songs.rb
+++ db/migrate/20201007231708_create_songs.rb
@@ -6,6 +6,8 @@ class CreateSongs < ActiveRecord::Migration[5.0]
       t.integer :release_year
       t.boolean :released
       t.string :genre
+
+       t.timestamps null: false
     end
   end
 end
