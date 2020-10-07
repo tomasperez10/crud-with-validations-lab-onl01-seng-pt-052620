0dcabd181723688af213b860b4c3c53b1dd5084e
Tomas Perez <tomasitopg6@gmail.com>
2020-10-07 23:17:08 +0000

    Automatically backed up by Learn

---
+++ db/migrate/20201007231708_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :songs do |t|
+      t.sting :title
+      t.string :artist_name
+      t.integer :release_year
+      t.boolean :released
+      t.string :genre
+    end
+  end
+end
